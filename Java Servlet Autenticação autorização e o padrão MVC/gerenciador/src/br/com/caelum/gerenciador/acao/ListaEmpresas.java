c6b534b40e4e343c5e3e31cc3309700de771a7f5
Andre Luis <adrluis7@gmail.com>
2020-12-11 20:27:55 -0300

    Testando o login

--- Java Servlet Autenticação autorização e o padrão MVC/gerenciador/src/br/com/caelum/gerenciador/acao/ListaEmpresas.java
+++ Java Servlet Autenticação autorização e o padrão MVC/gerenciador/src/br/com/caelum/gerenciador/acao/ListaEmpresas.java
@@ -6,21 +6,29 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.caelum.gerenciador.modelo.Banco;
 import br.com.caelum.gerenciador.modelo.Empresa;
 
-public class ListaEmpresas  implements Acao{
-	
-	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class ListaEmpresas implements Acao {
+
+	public String executa(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		System.out.println("listando empresas");
-
+		
+		HttpSession sessao = request.getSession();
+		
+		if (sessao.getAttribute("usuarioLogado") == null) {
+			return "redirect:entrada?acao=LoginForm";
+		}
+		
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
 
 		request.setAttribute("empresas", lista);
 
-        return "forward:listaEmpresas.jsp";
+		return "forward:listaEmpresas.jsp";
 	}
 }
