2ea6a2634dd2725205bece3324229846ef5d8ec1
Andre Luis <adrluis7@gmail.com>
2020-12-11 17:16:49 -0300

    Verificando o login

--- Java Servlet Autenticação autorização e o padrão MVC/gerenciador/src/br/com/caelum/gerenciador/acao/Login.java
+++ Java Servlet Autenticação autorização e o padrão MVC/gerenciador/src/br/com/caelum/gerenciador/acao/Login.java
@@ -6,18 +6,28 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.caelum.gerenciador.modelo.Banco;
+import br.com.caelum.gerenciador.modelo.Usuario;
+
 public class Login implements Acao {
 
 	@Override
 	public String executa(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
 
-		System.out.println("Logando " + login);
+		Banco banco = new Banco();
+		Usuario usuario = banco.existeUsuario(login, senha);
 
-		return "redirect:entrada?acao=ListaEmpresas";
+		if (usuario != null) {
+			System.out.println("Usuario existe");
+			return "redirect:entrada?acao=ListaEmpresas";
+		} else {
+			return "redirect:entrada?acao=LoginForm";
+
+		}
 	}
 
 }
